1820a15ea2cf58d249feab56afdc43c293bc5b66
asv02 <200106025@hbtu.ac.in>
2025-07-01 20:00:24 +0530

    added validation on user

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema(
     {
+        photoUrl:
+        {
+            type:String,
+            validate(value)
+            {
+                if(!validator.isURL(value))
+                    {
+                        throw new Error('Enter valid photo url');
+                    }
+            }
+        },
         firstName: {
-            type: String
+            type: String,
+            required:true,
+            uppercase:true
         },
         lastName:
         {
@@ -11,17 +25,51 @@ const userSchema = new mongoose.Schema(
         },
         emailId:
         {
-            type: String
+            type: String,
+            required:true,
+            lowercase: true,
+            unique:true,
+            validate(value)
+            {
+                if(!validator.isEmail(value))
+                    {
+                        throw new Error('Email Address Invalid!')
+                    }
+            }
         },
         passWord:
         {
-            type: String
+            type: String,
+            required:true,
+            minLength:8,
+            validate(value)
+            {
+                if(!validator.isStrongPassword(value))
+                    {
+                        throw new Error("Password must be of at least length 8 and include 1 uppercase and 1 lowercase character");
+                    }
+            }
         },
         gender:
         {
-            type: String
+            type: String,
+            required:true,
+            validate(value)
+            {
+                if(!['male','female','other'].includes(value))
+                    {
+                        throw new Error('Not valid Gender')
+                    }
+            },
+            lowercase:true,
+            
+        },
+        about:
+        {
+            type: String,
+            default:"Nothing"
         }
-    })
+    },{ timestamps: true })
 
 const User = mongoose.model('User', userSchema)
 module.exports = User;
